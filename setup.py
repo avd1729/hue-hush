b8cfc32c04c8dd22fd29c9744a52b56a01f38e4b
avd <aravind19112003@gmail.com>
2024-02-12 21:07:16 +0530

    hue hush v 0.0.1

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     description = fh.read()
 
 setuptools.setup(
-    name="chroma_squeeze",
-    version="0.0.3",
+    name="huehush",
+    version="0.0.1",
     author="Aravind.M.S",
     author_email="aravind19112003@gmail.com",
-    packages=["chroma_squeeze"],
-    description="chroma_squeeze is a Python module that provides functionality to segment images into distinct clusters using the K-means clustering algorithm. This module is useful for tasks such as image segmentation, color quantization, and image compression.",
+    packages=["huehush"],
+    description="huehush is a Python module that provides functionality to segment images into distinct clusters using the K-means clustering algorithm. This module is useful for tasks such as image segmentation, color quantization, and image compression.",
     long_description=description,
     long_description_content_type="text/markdown",
-    url="https://github.com/avd1729/Chroma-Squeeze",
+    url="https://github.com/avd1729/hue-hush",
     license='Apache License 2.0',
     python_requires='>=3.8',
     install_requires=[
